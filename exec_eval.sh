bc93c4f377c79a7f978da625e61a61bba2b5ea82
Sebastian Bücker <sebastian.buecker@stud.tu-darmstadt.de>
2019-06-25 16:01:47 +0200

    update

--- exec_eval.sh
+++ exec_eval.sh
@@ -8,8 +8,8 @@ for task in $Tasks; do
         python generate_shuffled.py -gs DailyDialog -m $task
         
         python train_models.py -g DailyDialog -m $task
-        svm_learn -z p experiments/DailyDialog/reordering/$task/DailyDialog_sal1_range2_2_train.dat
-        svm_classify experiments/Oasis/reordering/$task/DailyDialog_sal1_range2_2_test.dat "${test}_model" "${test}_prediction"
+        svm_learn -z p experiments/DailyDialog/reordering/$task/DailyDialog_sal1_range2_2_train.dat "${task}_model"
+        svm_classify experiments/DailyDialog/reordering/$task/DailyDialog_sal1_range2_2_test.dat "${task}_model" "${task}_prediction"
         
-        python eval_svmlight_output.py --testfile experiments/DailyDialog/reordering/$task/DailyDialog_sal1_range2_2_test.dat --predfile "${test}_prediction"
+        python eval_svmlight_output.py --testfile experiments/DailyDialog/reordering/$task/DailyDialog_sal1_range2_2_test.dat --predfile "${task}_prediction" >> logs/${task}_result.txt
 done
